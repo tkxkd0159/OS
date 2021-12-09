b40c2a1b5718f28eaffc9ae488c1adae98181800
Jaeseung Lee(KU) <41176085+tkxkd0159@users.noreply.github.com>
2021-12-09 15:09:11 +0900

    Add comment on Multichat program

--- multi_chat/client.c
+++ multi_chat/client.c
@@ -12,17 +12,17 @@
 
 #define CHAT_LEN 2048
 #define BUF_SIZE CHAT_LEN+64
-volatile sig_atomic_t FLAG = 0; // 시그널 핸들러를 위한 전역변수
+volatile sig_atomic_t FLAG = 0;       // 시그널 핸들러를 위한 전역변수
 
-void str_trim_lf(char* arr, int len);
-void chat_prefix();
-void sigint_handler();
-void send_msg_handler(void* sockfd);
-void recv_msg_handler(void* sockfd);
+void str_trim_lf(char* arr, int len); // 문자열 개행문자 제거
+void chat_prefix();                   // 채팅창에서 내 메시지 구분을 위한 prefix 생성 함수
+void sigint_handler();                // ctrl+c나 내가 임의로 거는 인터럽트 조건에서 실행시키기 위한 핸들러
+void send_msg_handler(void* sockfd);  // 메시지를 보낼 때 이를 처리할 핸들러
+void recv_msg_handler(void* sockfd);  // 메시지를 받을 때 이를 처리할 핸들러
 
 
 int main() {
-    signal(SIGINT, sigint_handler);
+    signal(SIGINT, sigint_handler);   // ctrl+c(SIGINT) 처리를 위한 핸들러 등록
 
     char addr[20];
     int port;
@@ -34,13 +34,14 @@ int main() {
     printf("open!! client\n");
 
     struct sockaddr_in serv_addr;
+
     // server.c에서 설정해줬던 소켓과 동일하게 설정
-    int sockfd = socket(PF_INET, SOCK_STREAM, 0); // IPv4 프로토콜 연결 지향형 소켓 생성 (TCP)
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(addr);
     serv_addr.sin_port = htons(port);
 
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){ // 서버에 연결
         printf("ERROR : connection failed\n");
         return EXIT_FAILURE;
     } else {
@@ -49,27 +50,27 @@ int main() {
     }
 
     char msg[CHAT_LEN] = "";
-    int recv_bytes = recv(sockfd, msg, CHAT_LEN, 0);
+    int recv_bytes = recv(sockfd, msg, CHAT_LEN, 0); // 서버 환영 메시지를 처리하기 위한 recv
     if (recv_bytes > 0){
         printf("%s\n", msg);
     }
-    memset(msg, 0, CHAT_LEN);
+    memset(msg, 0, CHAT_LEN);     // 메시지 버퍼 초기화
     printf("input 'Q' to exit\n");
 
     pthread_t send_msg_thread;
     pthread_t recv_msg_thread;
-    if (pthread_create(&send_msg_thread, NULL, (void*)send_msg_handler, (void*)&sockfd) != 0){
+    if (pthread_create(&send_msg_thread, NULL, (void*)send_msg_handler, (void*)&sockfd) != 0){ // 메시지 송신 전용 스레드 생성
         printf("ERROR : pthread can't be generated\n");
         return EXIT_FAILURE;
     }
-    if (pthread_create(&recv_msg_thread, NULL, (void*)recv_msg_handler, (void*)&sockfd) != 0){
+    if (pthread_create(&recv_msg_thread, NULL, (void*)recv_msg_handler, (void*)&sockfd) != 0){ // 메시지 수신 전용 스레드 생성
         printf("ERROR : pthread can't be generated\n");
         return EXIT_FAILURE;
     }
 
 
     while(1){
-        if(FLAG){
+        if(FLAG){ // 인터럽트 발생 시 핸들러에 의해 FLAG 설정 후 루프 탈출
             printf("\nLeave chat room\n");
             break;
         }
@@ -110,7 +111,7 @@ void send_msg_handler(void* arg){
     while(1){
         fgets(msg, CHAT_LEN, stdin);
         str_trim_lf(msg, CHAT_LEN);
-        if (strcmp(msg, "Q") == 0){
+        if (strcmp(msg, "Q") == 0){  // Q 입력을 캐치 후 루프 탈출
             break;
         }
         else {
@@ -118,10 +119,10 @@ void send_msg_handler(void* arg){
             send(sockfd, buffer, strlen(buffer), 0);
             chat_prefix();
         }
-        memset(msg, 0, CHAT_LEN);
-        memset(buffer, 0, BUF_SIZE);
+        memset(msg, 0, CHAT_LEN);     // 입력 메시지 버퍼 초기화
+        memset(buffer, 0, BUF_SIZE);  // 전송 메시지 버퍼 초기화
     }
-    // Q 입력 시 종료를 위한 핸들러 호출
+    // FLAG 변화를 위한 핸들러 호출
     sigint_handler();
 }
 
@@ -130,11 +131,11 @@ void recv_msg_handler(void* arg){
     char msg[CHAT_LEN] = "";
     while(1){
         int recv_bytes = recv(sockfd, msg, CHAT_LEN, 0);
-        if (recv_bytes > 0){
+        if (recv_bytes > 0){  // 정상적으로 메시지 수신 시 처리
             printf("%s", msg);
             chat_prefix();
         }
-        else if (recv_bytes == 0){
+        else if (recv_bytes == 0){  // 연결된 서버가 종료 시
             break;
         }
         else {
--- multi_chat/server.c
+++ multi_chat/server.c
@@ -25,15 +25,15 @@ typedef struct{
 
 client_t* clients[MAX_CLIENTS];
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER; // 연결된 클라이언트 처리 시 각 액션의 독점 처리 보장을 위한 뮤텍스 설정
-void queue_add(client_t *cl);
-void queue_remove(int uid);
+void queue_add(client_t *cl);                              // 접속한 클라이언트를 관리 큐에 넣음
+void queue_remove(int uid);                                // 종료된 클라이언트를 관리 큐에서 뺌
 
 void str_trim_lf(char arr[], int len);
-void print_ip_addr(struct sockaddr_in addr);
-void flood_msg(char *msg, int from_uid);
-void send_msg(char *msg, client_t *cl);
-void* handle_client(void* arg);
-int choose_uid();
+void print_ip_addr(struct sockaddr_in addr); // 리틀엔디안으로 저장된 주소값을 시프트연산자를 통해 처리 후 출력
+void flood_msg(char *msg, int from_uid);     // sender를 제외한 모든 연결된 클라이언트에게 메세지 전송
+void send_msg(char *msg, client_t *cl);      // 특정 클라이언트에게 메세지 전송
+void* handle_client(void* arg);              // 클라이언트와 연결된 후 스레드 할당 시 어떤 일을 처리할 건지 정의
+int choose_uid();                            // 클라이언트가 서버에 접속 시 적절한 id 부여
 
 int main(int argc, char* argv[]) {
     const char *s_ip = "127.0.0.1";
@@ -86,8 +86,10 @@ int main(int argc, char* argv[]) {
     // 대기하다가 연결 요청이 오면 수락
     while(1) {
         socklen_t cliaddr_len = sizeof(cli_addr);
-        cli_sock = accept(serv_sock, (struct sockaddr*)&cli_addr, &cliaddr_len);
+        cli_sock = accept(serv_sock, (struct sockaddr*)&cli_addr, &cliaddr_len);  // 루프돌다가 클라이언트가 연걸 시도 시 수락
 
+
+        // 내가 정해놓은 최대 클라이언트 수 넘어가면 에러메시지를 클라이언트에게 보내고 소켓 닫음
         if (cli_cnt == MAX_CLIENTS) {
             printf("ERROR : Maximum clients connected\n");
             print_ip_addr(cli_addr);
@@ -97,12 +99,12 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        client_t *cli = (client_t*)malloc(sizeof(client_t)); // 런타임에서 클라이언트에 대한 메모리 할당
+        client_t *cli = (client_t*)malloc(sizeof(client_t)); // 런타임에서 클라이언트에 대한 메모리 할당, 아래에서 클라리언트 정보 구조체에 입력
         cli->addr = cli_addr;
         cli->sockfd = cli_sock;
         cli->uid = choose_uid();
         queue_add(cli);
-        pthread_create(&tid, NULL, &handle_client, (void*)cli);
+        pthread_create(&tid, NULL, &handle_client, (void*)cli); // 해당 클라이언트를 처리할 스레드 생성
 
         sleep(1);
 
@@ -122,19 +124,19 @@ void str_trim_lf(char* arr, int len) {
 }
 
 void queue_add(client_t *cl){
-    pthread_mutex_lock(&clients_mutex);
+    pthread_mutex_lock(&clients_mutex); // 공유변수 clients에 한번에 한 스레드만 접근하게 뮤텍스 락 건다
     for (int i=0; i<MAX_CLIENTS; i++){
         if(!clients[i]){
             clients[i] = cl;
             break;
         }
     }
-    pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_unlock(&clients_mutex); // 뮤텍스 락 해제
 
 }
 
 void queue_remove(int uid){
-    pthread_mutex_lock(&clients_mutex);
+    pthread_mutex_lock(&clients_mutex);  // 공유변수 clients에 한번에 한 스레드만 접근하게 뮤텍스 락 건다
     for (int i=0; i<MAX_CLIENTS; i++){
         if(clients[i]){
             if(clients[i]->uid == uid){
@@ -144,12 +146,11 @@ void queue_remove(int uid){
         }
     }
 
-    pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_unlock(&clients_mutex);  // 뮤텍스 락 해제
 
 }
 
 
-// 리틀엔디안으로 저장된 주소값을 시프트연산자를 통해 처리 후 출력
 void print_ip_addr(struct sockaddr_in addr) {
     printf("SRC : %d.%d.%d.%d\n",
             addr.sin_addr.s_addr & 0xff,
@@ -158,10 +159,10 @@ void print_ip_addr(struct sockaddr_in addr) {
             (addr.sin_addr.s_addr & 0xff000000) >> 24);
 }
 
-// sender를 제외한 모든 연결된 클라이언트에게 메세지 전송
+
 void flood_msg(char* msg, int from_uid){
-    pthread_mutex_lock(&clients_mutex);
-    for (int i=0; i<MAX_CLIENTS; i++){
+    pthread_mutex_lock(&clients_mutex); // 공유변수 clients에 한번에 한 스레드만 접근하게 뮤텍스 락 건다
+    for (int i=0; i<MAX_CLIENTS; i++){  // uid를 근거로 자신을 제외한 나머지 접속한 클라이언트에게 메시지 보냄
         if(clients[i]){
             if(clients[i]->uid != from_uid){
                 if(write(clients[i]->sockfd, msg, strlen(msg)) < 0){
@@ -171,16 +172,14 @@ void flood_msg(char* msg, int from_uid){
             }
         }
     }
-    pthread_mutex_unlock(&clients_mutex);
+    pthread_mutex_unlock(&clients_mutex); // 뮤텍스 락 해제
 }
 
-// 특정 클라이언트에게 메세지 전송
+
 void send_msg(char* msg, client_t *cl){
-    pthread_mutex_lock(&clients_mutex);
-    if (write(cl->sockfd, msg, strlen(msg)) < 0){
+    if (write(cl->sockfd, msg, strlen(msg)) < 0){  // 원하는 uid에게 메시지 전송
         printf("ERROR : write to descriptor failed\n");
     }
-    pthread_mutex_unlock(&clients_mutex);
 }
 
 void* handle_client(void* arg){
@@ -189,6 +188,7 @@ void* handle_client(void* arg){
     int exit_flag = 0;
     cli_cnt++;
 
+    // 첫 접속 시 접속한 클라이언트에게 환영메시지를 보내고 이미 접속되있는 클라이언트들에게는 해당 클라이언트 접속을 알림
     client_t* cli = (client_t*)arg;
     printf("connected to Client %d\n", cli->uid);
     sprintf(buffer, "\rServer : Client %d has been connected\n", cli->uid);
@@ -197,12 +197,13 @@ void* handle_client(void* arg){
     memset(buffer, 0, BUF_SIZE);
 
     while(1){
+        // 내가 설정한 조건들에 의해 exit_flag가 설정되면 루프 탈출
         if (exit_flag){
             break;
         }
 
         int recv_bytes = recv(cli->sockfd, buffer, BUF_SIZE, 0);
-        if (recv_bytes > 0){
+        if (recv_bytes > 0){   // 일반적인 메시지 수신 시 조건
             if(strlen(buffer) > 0){
                 str_trim_lf(buffer, strlen(buffer));
                 sprintf(flood_buffer, "\rClient %d : %s\n", cli->uid, buffer);
@@ -210,7 +211,7 @@ void* handle_client(void* arg){
                 printf("message from client %d : %s\n", cli->uid, buffer);
             }
         }
-        else if (recv_bytes == 0 || strcmp(buffer, "Q") == 0){
+        else if (recv_bytes == 0){ // 클라이언트에서 접속 종료 시 조건
             sprintf(buffer, "\rServer : Client %d has left\n", cli->uid);
             printf("Client %d has left\n", cli->uid);
             flood_msg(buffer, cli->uid);
@@ -220,15 +221,15 @@ void* handle_client(void* arg){
             printf("ERROR : Client handling failed\n");
             exit_flag = 1;
         }
-        memset(buffer, 0, BUF_SIZE);
-        memset(flood_buffer, 0, BUF_SIZE+16);
+        memset(buffer, 0, BUF_SIZE);            // 버퍼 초기화
+        memset(flood_buffer, 0, BUF_SIZE+16);   // 플러딩용 버퍼 초기화
     }
-    close(cli->sockfd);
-    queue_remove(cli->uid);
+    close(cli->sockfd);                      // 해당 소켓 종료
+    queue_remove(cli->uid);                  // 해당 클라이언트 관리큐에서 제거
     uids[cli->uid-1] = 0;
-    free(cli);
+    free(cli);                               // 해당 클라이언트에 할당된 메모리 해제
     cli_cnt--;
-    pthread_detach(pthread_self());
+    pthread_detach(pthread_self());          // 스레드 리소스 반환
 
     return NULL;
 }
